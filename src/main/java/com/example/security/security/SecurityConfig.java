52056de23b56d29b31259aa950e586d8b68b1dc0
dali05 <dali.laaroussi@gmail.Com>
2024-02-03 01:08:27 +0100

    default configuration of spring security

--- src/main/java/com/example/security/security/SecurityConfig.java
+++ src/main/java/com/example/security/security/SecurityConfig.java
@@ -1,32 +1,9 @@
 package com.example.security.security;
 
-import com.example.security.security.filters.CustomAuthenticationFilter;
-import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@AllArgsConstructor
 public class SecurityConfig {
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    private final CustomAuthenticationFilter customAuthenticationFilter;
-
-    @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .addFilterAt(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .authorizeRequests().anyRequest().authenticated()
-                .and().build();
-    }
 
 }
